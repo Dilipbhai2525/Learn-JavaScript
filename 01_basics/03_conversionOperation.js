0e15ef81af5d7f5b6c64ac80eeb8db824365fac0
Dilip Chaudhary <dilipbhaichaudhari2525@gmail.com>
2025-07-27 10:43:24 +0530

    chep 03 conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,37 @@
+// let score = 33;
+// let score = "33";
+let score = "33abc";
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// NaN is special Type in JavaScript
+// console.log(isNaN(valueInNumber)); // true if valueInNumber is NaN
+
+// "33" => convert to number 33
+// "33abc" => convert to NaN
+// "abc" => convert to NaN
+
+// let isLoggedIn = 1;
+// let isLoggedIn = 0;
+// let isLoggedIn = "";
+// let isLoggedIn;
+let isLoggedIn = "dilip";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true
+// 0 => false
+// sting => true
+// emty => false
+// null => false
+
+let someNumber = 33;
+let numis = String(someNumber);
+console.log(numis);
+console.log(typeof numis);
+
+
